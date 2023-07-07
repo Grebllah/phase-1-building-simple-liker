8b2735ed7e11a8a5a7cd3fa9ba801469600fe378
James Hallberg <Grebllah81788@gmail.com>
2023-07-07 15:11:38 -0400

    complete simple liker lab

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+modal.setAttribute("class", "hidden")
+console.log(modal)
+const likeButtons = document.querySelectorAll(".like-glyph")
+function handleClick (e) {
+const heart = e.target
+  // console.log(mimicServerCall)
+  mimicServerCall("fakeURL")
+  .then(() => {
+    console.log(heart)
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className = "activated-heart"
+    }
+    else {heart.innerText = EMPTY_HEART
+    heart.className = ""
+    }
+  })
+  .catch((error) => {
+    // find element
+    modal.className = ""
+    modal.innerText = error
+    setTimeout(() => modal.className = "hidden", 3000)
+    // set class off of hidden
+    // throw new Error("Fail")
+  })
+}
 
+likeButtons.forEach(likeButton => likeButton.addEventListener("click", handleClick))
 
 
 
